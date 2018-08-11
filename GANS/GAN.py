d1f0727d103ffb46684dcb1f9da890070bb6186e
zhc <zhc199611@qq.com>
2018-08-12 00:07:15 +0800

    create TEST dir

---
+++ GANS/GAN.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+import torch
+import torch.optim
+from torchvision import datasets,transforms
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+
+
+
+
+
+
--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,4 +1,4 @@
-
+# -*- coding: UTF-8 -*-
 import torch
 import torch.optim
 from torchvision import datasets,transforms
